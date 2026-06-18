d835ed7cc885d8e2137094dea3835df636c87d9b
HiiPower05 <fordarkseid13@gmail.com>
2026-06-18 12:44:19 +0200

    Added logout.php and navigation.

---
+++ index.php
@@ -0,0 +1,21 @@
+<link rel="stylesheet" href="styles/style.css">
+
+<?php
+session_start();
+include 'nav.php';
+?>
+
+<h1>Pastimes Clothing Store</h1>
+
+<p>
+Buy and sell quality second-hand clothing.
+</p>
+
+<p>
+Browse clothing items, place orders, manage your cart,
+and communicate with administrators.
+</p>
+
+<a href="items.php">
+Browse Available Clothes
+</a>
